8867a28d4c116cdbcd664473e721bb959f54f64e
Tim Graham <tomgus1.2@gmail.com>
2017-07-11 03:40:49 +0200

    SELINUX: Use Enforcing SELINUX
    Change-Id: I42ba93e82f0c8a729c8a1844601ed1473c2ee2f6

--- CommonConfig.mk
+++ CommonConfig.mk
@@ -26,9 +26,9 @@ TARGET_NO_RECOVERY := false
 TARGET_NO_KERNEL := false
 
 # common cmdline parameters
-ifneq ($(BOARD_USE_ENFORCING_SELINUX),true)
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-endif
+#ifneq ($(BOARD_USE_ENFORCING_SELINUX),true)
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+#endif
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += dwc3.maximum_speed=high dwc3_msm.prop_chg_detect=Y
