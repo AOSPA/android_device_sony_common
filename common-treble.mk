e5c11b00d9194cd59d3d9dea1829e4a3b84fb820
Tim <tomgus1.2@gmail.com>
2018-05-25 07:51:22 +0200

    Revert: common: binderize sensors HAL
    This reverts commit 0dbac4b disabling the sensor service allowing sensors to work again on PA
    
    Change-Id: I7bb9ccfa7ccfcbab017c3fc1f7cf9f94f94f87d7

--- common-treble.mk
+++ common-treble.mk
@@ -64,8 +64,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-impl
 
 # Vibrator
 PRODUCT_PACKAGES += \
