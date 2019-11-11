f3e8744bca29eaf63e097590cd3b1ecf2961841b
Tim <tomgus1.2@gmail.com>
2019-11-11 17:12:56 +0100

    Revert "common-treble/vintf: Remove widevine refs"
    This reverts commit 9c7e34f68764dbad502f0ee9c0077ec2e733d3c6.

--- common-treble.mk
+++ common-treble.mk
@@ -104,7 +104,8 @@ endif
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.2-service.clearkey \
+    android.hardware.drm@1.1-service.widevine
 
 ifneq ($(BOARD_USE_LEGACY_USB),true)
 # Usb HAL
