81736ad352ae361d748fac6edfd5d14a5c810067
Tim <tomgus1.2@gmail.com>
2020-03-02 15:27:54 +0100

    test for updates

--- hardware/qcom/Android.mk
+++ hardware/qcom/Android.mk
@@ -15,7 +15,7 @@ MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845 sm8150
 
 audio-hal := hardware/qcom/audio
 gps-hal := hardware/qcom/gps/sdm845
-ipa-hal := hardware/qcom/data/ipacfg-mgr/sdm845
+#ipa-hal := hardware/qcom/data/ipacfg-mgr/sdm845
 
 display-hal := hardware/qcom/display/sde
 
@@ -37,7 +37,7 @@ include device/sony/common/hardware/qcom/utils.mk
 
 include $(display-hal)/Android.mk
 include $(call all-makefiles-under,$(audio-hal))
-include $(call first-makefiles-under,$(ipa-hal))
+#include $(call first-makefiles-under,$(ipa-hal))
 include $(call all-makefiles-under,$(gps-hal))
 include $(call all-makefiles-under,$(media-hal))
 endif
